4c92d265628b0f9beea646983d10f5586ca9d288
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-01-05 14:10:37 +0000

    First clean-up work on the TEMPLATES AND UTILITIES section of the components library from audit excel sheet.

--- stories/Utilities/FrostedImage/FrostedImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './frosted-background.scss';
-import BackgroundImg from '../../assets/images/bees.jpg';
-
-export const FrostedImage = () => (
-  <div className="frosted-background">
-    <img src={BackgroundImg} alt={BackgroundImg} />
-  </div>
-);
