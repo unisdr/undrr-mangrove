c39cd573835c54a40836ecd098f25d6bb08eb1bc
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-01-05 14:05:51 +0000

    First clean-up work on the ORGANISM AND PATTERNS section of the components library from audit excel sheet.

--- stories/Organism/Blocks/FeaturedContentCard/FeaturedCard/FeaturedCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './featured-card.scss';
-import { FeaturedContentCard } from '../../../../Components/UIcomponents/Cards/FeaturedCard/FeaturedContentCard';
-
-export const FeaturedCard = ({ headertext, data }) => (
-  <div className="featured-card-container">
-    <FeaturedContentCard headertext={headertext} data={data} />
-  </div>
-);
