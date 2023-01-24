963808577c60a71d1caf0f6136f30217d464c13e
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-01-24 13:16:58 +0000

    Card and Hero component alphas (#3)
    * Initial Integration work for Mangrove Card and Hero component.
    
    * fix wrong class name and fullwidtth behaviour.
    
    * change Hero field subtitle to summaryTexT.
    
    * Remove UNDP Hero and refactor style.scs.
    
    * Rename UIComponent MangroveHero to Hero and
    refactor codebase.
    
    * change contentname to title, text to summaryText,
    and refactor code for Card component.
    
    * further refinement of css classes for integration
    with Gutenberg.

---
+++ stories/Components/UIcomponents/Cards/MangroveCard/HorizontalCard.jsx
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react';
+import './card.scss';
+import '../../../../assets/scss/_grid.scss';
+
+const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
+
+export const hovercolors_options = {
+  yellow: '',
+  red: 'red',
+  blue: 'blue',
+  green: 'green',
+};
+
+export function HorizontalCard({
+  data, Hovercolors,
+}) {
+  let hovercolors_variant = hovercolors_options[`${Hovercolors}`];
+  return (
+    <>
+      <h5>HORIZONTAL CARD</h5>
+      <article className="mg-card">
+
+        {data.map((item, index) => (
+          <div key={index} className="mg-card__hc">
+            <div className="mg-card__visual">
+              <img src={item.imgback} alt={item.imgback} className="mg-card__image" />
+              <a href={item.link} className="mg-card__share">{item.share}</a>
+            </div>
+
+            <div className={cls('mg-card__content', `${hovercolors_variant}`)}>
+
+              <div className="mg-card__meta">
+                <a href={item.link} className="mg-card__label mg-card__label--active">{item.label1}</a>
+                <a href={item.link} className="mg-card__label mg-card__label--active">{item.label2}</a>
+              </div>
+
+              <header className="mg-card__title">
+                <a href={item.link}>{item.title}</a>
+              </header>
+
+              <p className="mg-card__summaryText">{item.summaryText}</p>
+
+              <a href={item.link} className="mg-card__button">{item.button}</a>
+
+            </div>
+
+          </div>
+        ))}
+      </article>
+    </>
+  );
+}
+
+HorizontalCard.defaultProps = {
+  Hovercolors: 'yellow',
+};
---
+++ stories/Components/UIcomponents/Hero/ChildHero.jsx
@@ -0,0 +1,58 @@
+import React, { Fragment } from 'react';
+import './hero.scss';
+import '../../../assets/scss/_grid.scss';
+import imgPath from '../../../assets/images/hero_background.png';
+
+const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
+let heroStyle = {
+  backgroundImage: `url(${imgPath})`,
+};
+
+export const hovercolors_options = {
+  yellow: '',
+  red: 'red',
+  blue: 'blue',
+  green: 'green',
+};
+
+export function ChildHero({
+  data, Hovercolors,
+}) {
+  let hovercolors_variant = hovercolors_options[`${Hovercolors}`];
+  return (
+
+    <>
+      <h5>HERO SUB (Child kitchen sink)</h5>
+      <section className="mg-hero" style={heroStyle}>
+
+        {data.map((item, index) => (
+          <div key={index} className="mg-hero__overlay child">
+
+            <article className="mg-hero__content">
+
+              <div className="mg-hero__meta">
+                <a href={item.link} className="mg-hero__label">{item.label}</a>
+              </div>
+
+              <header className="mg-hero__title">
+                <a href="#" className="text-xxl">{item.title}</a>
+              </header>
+
+              <div className="mg-hero__summaryText">{item.summaryText}</div>
+
+              <div className="mg-hero__buttons">
+                <a href={item.link} className="mg-hero__button mg-hero__button-primary">{item.primary_button}</a>
+              </div>
+
+            </article>
+
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}
+
+ChildHero.defaultProps = {
+  Hovercolors: 'yellow',
+};
