66bbd7d5bf4e9ccb4708631ca13cf9119ca9a257
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-01-26 23:49:56 +0000

    Restore links to summaryText field and refactor code.

--- stories/Components/UIcomponents/Cards/MangroveCard/HorizontalCard.jsx
+++ stories/Components/UIcomponents/Cards/MangroveCard/HorizontalCard.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/no-danger */
 import React, { Fragment } from 'react';
+import DOMPurify from 'dompurify';
+
 import './card.scss';
 import '../../../../assets/scss/_grid.scss';
 
@@ -38,7 +41,7 @@ export function HorizontalCard({
                 <a href={item.link}>{item.title}</a>
               </header>
 
-              <p className="mg-card__summaryText">{item.summaryText}</p>
+              <p className="mg-card__summaryText" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.summaryText) }} />
 
               <a href={item.link} className="mg-card__button">{item.button}</a>
 
