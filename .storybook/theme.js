92dc237790c6ae80013a83be745bcb81ca2b9a22
Ken Hawkins <khawkins98@gmail.com>
2023-01-27 13:31:11 +0100

    Chore: cleanup old github references to point to unisdr url (#5)

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'UNDRR Mangrove',
-  brandUrl: 'https://khawkins98.github.io/undrr-mangrove',
+  brandUrl: 'https://unisdr.github.io/undrr-mangrove',
   brandImage: './images/undrr-logo-blue.svg'
 });
