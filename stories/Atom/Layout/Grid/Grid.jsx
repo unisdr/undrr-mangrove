f5b7c9a71bc4940623dcee840d07c0ecf548a9ba
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-02-21 15:39:36 +0000

    add grid system to the component library.

--- stories/Atom/Layout/Grid/Grid.jsx
+++ stories/Atom/Layout/Grid/Grid.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
 // import './grid.scss';
+import "../../../assets/scss/_grid.scss"
 
 export function Grid() {
   return (
-    <p>To be determined...</p>
+    <>
+      <h1>VARIANTS</h1>
+      <hr />
+
+      <h2>1: Auto Layout (mg-grid)</h2>
+
+      <div class="mg-grid">
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+      </div>
+
+      <h2>2: Specified Columns Layout - (example - a two-column grid: mg-grid__col-2)</h2>
+
+      <div class="mg-grid mg-grid__col-2">
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div class="mg-grid__content">
+          <h3 class="mg-grid__heading">UNDRR</h3>
+          <p class="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+      </div>
+      
+      <div class="mg-grid mg-grid__col-3">
+      </div>
+
+    </>
+
   );
 }
