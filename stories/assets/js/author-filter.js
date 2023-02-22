6def5f216c3165ff84af679ce82f24c7e9afc8c2
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-02-22 15:34:31 +0000

    cleanup js files and refactor works.

--- stories/assets/js/author-filter.js
+++
@@ -1,6 +0,0 @@
-export const authorFilter = () => {
-  jQuery('.sort-filter').click(function sortclick() {
-    jQuery(this).toggleClass('close');
-    jQuery('.author-filter').toggleClass('author-filter-show');
-  });
-};
