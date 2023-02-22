f47cc132f3cfa37ac63d55fc172f432561cf1be7
Alex Mensah Tenkorang <tamensah@staff.ug.edu.gh>
2023-02-22 12:53:36 +0000

    add grid system to the component library. (#30)
    * add grid system to the component library.

--- stories/Atom/Layout/Grid/Grid.jsx
+++ stories/Atom/Layout/Grid/Grid.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
-// import './grid.scss';
+import './grid.scss';
+
 
 export function Grid() {
   return (
-    <p>To be determined...</p>
+    <>
+      <h1>VARIANTS</h1>
+      <hr />
+
+      <h2>1: Auto Layout (mg-grid)</h2>
+
+      <div className="mg-grid">
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+      </div>
+
+      <h2>2: Specified Columns Layout - (example - a two-column grid: mg-grid__col-2)</h2>
+
+      <div className="mg-grid mg-grid__col-2">
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+        <div className="mg-grid__content">
+          <h3 className="mg-grid__heading">UNDRR</h3>
+          <p className="mg-grid__text">UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
+        </div>
+
+      </div>
+      
+      <div className="mg-grid mg-grid__col-3">
+      </div>
+
+    </>
+
   );
 }
--- stories/Components/UIcomponents/Cards/Card/HorizontalCard.jsx
+++ stories/Components/UIcomponents/Cards/Card/HorizontalCard.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import DOMPurify from 'dompurify';
 import { CtaButton } from '../../Buttons/CtaButton/CtaButton';
 // import './card.scss';
-import '../../../../assets/scss/_grid.scss';
+//import '../../../../assets/scss/_grid.scss';
 
 const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
 
