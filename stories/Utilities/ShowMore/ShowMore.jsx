964b0f8a72b181605506dfb4ae47c6880d2d9b50
Ken Hawkins <khawkins98@gmail.com>
2023-08-24 17:45:04 +0200

    undrr/web-backlog#1082: Add show more component.

---
+++ stories/Utilities/ShowMore/ShowMore.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { mgShowMore } from "../../assets/js/show-more";
+
+export function ShowMore({ data }) {
+  useEffect(() => {
+    mgShowMore();
+  }, []);
+  return (
+    <React.Fragment>
+      {" "}
+      {data.map((item, index) => (
+        <React.Fragment>
+          <div class={item.collapsable_wrapper_class}>
+            {item.collapsable_text}
+          </div>
+          <a
+            href="#"
+            class="mg-button mg-button-primary mg-show-more--button"
+            data-mg-show-more="true"
+            data-mg-show-more-target={`.${item.collapsable_wrapper_class}`}
+            data-mg-show-more-label-open="Show less themes"
+            data-mg-show-more-label-collapsed="Show more themes"
+          >
+            {item.button_text}
+          </a>
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  );
+}
---
+++ stories/assets/js/show-more.js
@@ -0,0 +1,38 @@
+// mg-show-more
+// https://gitlab.com/undrr/web-backlog/-/issues/1082
+
+export function mgShowMore() {
+  const mgShowMoreButtons = document.querySelectorAll("[data-mg-show-more]");
+
+  mgShowMoreButtons.forEach((item) => {
+    item.dataset.dataVfGoogleAnalyticsLabel =
+      "Show more: " + item.dataset.mgShowMoreLabelCollapsed || `Show more`;
+
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      let mgShowMoreTargetClass =
+        item.dataset.mgShowMoreTarget || ".mg-show-more--container";
+      let mgShowMoreTarget = document.querySelector(mgShowMoreTargetClass);
+      mgShowMoreTarget.classList.toggle("mg-show-more--collapsed");
+
+      // Which label to show?
+      item.textContent = mgShowMoreTarget.classList.contains(
+        "mg-show-more--collapsed"
+      )
+        ? item.dataset.mgShowMoreLabelCollapsed || "Show more"
+        : item.dataset.mgShowMoreLabelOpen || "Show less";
+
+      if (mgShowMoreTarget.classList.contains("mg-show-more--collapsed")) {
+        item.classList.remove("mg-show-more--button--open");
+      } else {
+        item.classList.add("mg-show-more--button--open");
+      }
+
+    });
+
+
+    item.click();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", mgShowMore, false);
