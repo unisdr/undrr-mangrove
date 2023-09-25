914fcb9aba62270acfb72b81f6fce1f9e9596ec3
Simon Vlcek <vlcek@cyber-guard.com>
2023-09-25 09:07:09 +0000

    Merged in feature/social-links-component
    * fix sharing buttons for urlBased sharing
    
    * Switched to themed .scss variables
    
    * fix size of mail Icon
    
    * customized background and hover variants for more themed look
    
    * updated docs of ShareButton
    
    * improved description of getSharedLink()
    
    * made changing links more managable and update docs
    
    * Refactor share fn
    
    * Add aria labels

---
+++ stories/Components/Buttons/ShareButtons/ShareButtons.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+// import './share-buttons.scss';
+import FacebookIconWhite from './FacebookIconWhite.svg';
+import TwitterIconWhite from './TwitterIconWhite.svg';
+import LinkedInIconWhite from './LinkedInIconWhite.svg';
+import MailIconWhite from './MailIconWhite.svg';
+import CopyIcon from './CopyIcon.svg';
+import CheckIcon from './CheckIcon.svg';
+import LinkIcon from './LinkIcon.svg';
+import LinkUrls from "./links.json"
+const defaults = {
+  defaultSharingTextBody: "Check out this link: ",
+  defaultSharingTextSubject: "Sharing Link",
+}
+/** 
+*  ShareButtons is a Component that renders buttons to share on platforms like twitter, facebook, linkedin and others
+*  @param {Object} labels - should contain language specific values like: 
+*                           mainLabel: "SHARE THIS "
+*                           onCopy: "Copied"    
+*  @param {string} SharingSubject - subject of the Email that will be prefilled to the user 
+*  @param {string} SharingTextBody - body of the Email that will be prefilled and appended with LINK to the site
+
+*/
+export function ShareButtons({
+  labels,
+  SharingSubject,
+  SharingTextBody,
+  // CustomUrl, 
+  ...props
+}) {
+
+
+  /** 
+   * for now it is getting the local url and not the Custom link
+   * TODO: getting the link that is shared- if not provided, get location href
+   *  let SharedLink = CustomLink
+   *  if ( CutomLink === '' || CutomLink === undefined || CutomLink === null) {
+   *    SharedLink = window.location.href;
+   *  }
+   *  else {
+   *    SharedLink = CutomLink;
+   *  }
+   * return SharedLink;
+   *
+ **/
+  const getLinkToShare = () => {
+    return window.location.href;
+  }
+
+  
+  const sharedLink = getLinkToShare()
+
+  /** 
+   * Function that returns Encoded URL with the prefilled Values from the links.json
+   *  @param {string} Platform - Platform that will determine the link url from links Json
+   * @returns string executeLink - example: https://twitter.com/intent/tweet?text=Look%20at%20this%3A%20http%3A%2F%2Flocalhost%3A6006%2Fiframe.html%3FviewMode%3Dstory%26id%3Dcomponents-buttons-sharebuttons--share-buttons%26args%3D
+ **/
+  const getShareableLinkForPlatform = (Platform) => {
+    let baseLink = LinkUrls[Platform];
+    const subject = encodeURIComponent(SharingSubject ?? defaults.defaultSharingTextSubject);
+    const body = encodeURIComponent(SharingTextBody ?? defaults.defaultSharingTextBody) + encodeURIComponent(sharedLink);
+  
+    return baseLink.replace("[SHARING_TEXT_SUBJECT]", subject).replace("[SHARING_TEXT_BODY]", body);
+  }
+
+  const handleClick = (Platform) => {
+    var executableLink = getShareableLinkForPlatform(Platform)
+
+    //  mail have different way to open the link
+    if (Platform === "Mail") {
+      window.location.href = executableLink; // Opens the default email client
+      return
+    } else {
+      window.open(executableLink, '_blank', 'width=600,height=300');
+    }
+
+  };
+
+  return (
+    <div className="mg-share-buttons-wrapper" {...props}>
+      <div className='mainLabel'>{labels.mainLabel}</div>
+      <div>
+        <button className="mg-share-button mg-button-primary" onClick={() => handleClick("Facebook")} aria-label='Share on Facebook' >
+          <img src={FacebookIconWhite} alt="Facebook SVG Image" />
+        </button>
+        <button className="mg-share-button mg-button-primary" onClick={() => handleClick("Twitter")} aria-label='Share on Twitter' >
+          <img src={TwitterIconWhite} alt="Twitter SVG Image" />
+
+        </button>
+        <button className="mg-share-button mg-button-primary" onClick={() => handleClick("LinkedIn")} aria-label='Share on LinkedIn' >
+          <img src={LinkedInIconWhite} alt="LinkedIn SVG Image" />
+
+        </button>
+        <button className="mg-share-button mg-button-primary" onClick={() => handleClick("Mail")} aria-label='Share via Email' >
+          <img src={MailIconWhite} alt="Mail SVG Image" />
+        </button>
+      </div>
+
+      <CopyButton copiedLabel={labels.onCopy} sharedLink={sharedLink} />
+    </div>
+  );
+}
+/** 
+*  CopyButton
+*  @param {string} coppiedLabel - the label that will be shown when the link is coppied(should be in the right language)
+*  @param {string} sharedLink - the link that will be coppied
+*/
+function CopyButton({ coppiedLabel, sharedLink }) {
+
+  const [coppied, setCoppied] = React.useState(false);
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(sharedLink);
+    setCoppied(true);
+  }
+  return (
+    <button className='copyLink' onClick={() => handleCopyLink()}>
+      <div className='copyIcon'>
+        <img src={LinkIcon} alt="Link Icon" />
+      </div>
+      <div className='copyText'>{coppied ? coppiedLabel : sharedLink}</div>
+      <div className='copyIcon'>
+
+        {
+          coppied ?
+            <img src={CheckIcon} alt="check Icon" /> :
+            <img src={CopyIcon} alt="copy Icon" />
+        }
+
+      </div>
+    </button>)
+}
+
