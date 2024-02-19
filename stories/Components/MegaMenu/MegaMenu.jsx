94f75e4d17792fb7b4428e18f67f529661df4a7e
svlcek-cge <contact@cyber-guard.co.uk>
2024-02-19 19:06:51 +0100

    Feature/preact finalize build (#44)
    * Include styles
    
    * Lint

--- stories/Components/MegaMenu/MegaMenu.jsx
+++ stories/Components/MegaMenu/MegaMenu.jsx
@@ -1,6 +1,6 @@
 import { h } from "preact";
 import { useState, useEffect } from "preact/hooks";
-//import "./megamenu.scss";
+import "./megamenu.scss";
 import { TopBar } from "./TopBar/TopBar";
 import { Sidebar } from "./TopBar/Sidebar";
 import { useBreakpoint } from "./TopBar/hook";
