860dee118d9639e8180710ab1b7e7fcbad4ec2fa
Zachariah Ngonyani <zachariahn@gmail.com>
2024-08-09 08:35:01 +0300

    feat: add map clustering

--- stories/Components/Map/Map.jsx
+++ stories/Components/Map/Map.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
 import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import * as esri from "esri-leaflet";
+import DOMPurify from "dompurify"; // Import DOMPurify
 import styles from "./map.module.scss";
 
-// fix leaflet's default icon issues with webpack
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
@@ -16,13 +17,9 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
-const MapComponent = ({ data, center = [20, 0], zoom = 2 }) => {
-  const [selectedEntry, setSelectedEntry] = useState(null);
-
-  // calculate the max value for scaling
+export default function Map({ data, center = [20, 0], zoom = 2 }) {
   const maxValue = Math.max(...data.map((entry) => entry.value));
 
-  // calculate icon size based on value, starting from a minimum size
   const calculateIconSize = (value) => {
     const minSize = 38;
     const maxSize = 100;
@@ -49,35 +46,24 @@ const MapComponent = ({ data, center = [20, 0], zoom = 2 }) => {
         attribution='&copy; <a href="https://www.arcgis.com/home/item.html?id=7d88506b6af64b02ba3c08dbf66d014b">ArcGIS</a> contributors'
         url="https://geoservices.un.org/arcgis/rest/services/ClearMap_WebTopo/MapServer/tile/{z}/{y}/{x}"
       />
-      {data.map((entry, index) => (
-        <Marker
-          key={index}
-          position={entry.coords}
-          icon={createLabelIcon(entry.label, entry.value)}
-          eventHandlers={{
-            click: () => {
-              setSelectedEntry(entry);
-            },
-          }}
-        >
-          {selectedEntry && selectedEntry.label === entry.label && (
-            <Popup
-              position={entry.coords}
-              onClose={() => setSelectedEntry(null)}
-            >
-              <div>
-                <strong>{entry.label}</strong>
-                <br />
-                {entry.value} commitments
-                <br />
-                <p>{entry.description}</p>
-              </div>
+      <MarkerClusterGroup>
+        {data.map((entry, index) => (
+          <Marker
+            key={index}
+            position={entry.coords}
+            icon={createLabelIcon(entry.label, entry.value)}
+          >
+            <Popup>
+              <div
+                style={{ padding: "5px", lineHeight: "1.6em" }}
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(entry.description),
+                }}
+              />
             </Popup>
-          )}
-        </Marker>
-      ))}
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
-};
-
-export default MapComponent;
+}
