10bc7dc755b99006f0d36e1f0c88f4bf655e6a10
Ken Hawkins <kenneth.hawkins@un.org>
2024-09-02 18:56:17 +0200

    Revert "Merge branch 'main' into feature/map"
    This reverts commit b2e66663737248c50f2a67faaf89b3f2c88a78b0, reversing
    changes made to de27323edc8cfa4b5a8a37ff0f9fbeee83295b11.

--- stories/Components/MegaMenu/MegaMenu.jsx
+++ stories/Components/MegaMenu/MegaMenu.jsx
@@ -1,5 +1,5 @@
-import { h } from "preact";
-import { useState, useEffect } from "preact/hooks";
+import { useState, useEffect, useRef } from "preact/hooks";
+import "./megamenu.scss";
 import { TopBar } from "./TopBar/TopBar";
 import { Sidebar } from "./TopBar/Sidebar";
 import { useBreakpoint } from "./TopBar/hook";
