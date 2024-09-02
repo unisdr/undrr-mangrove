cdbd0c10447c84c30338ff6df7a4d7a0b1df6c37
Zachariah Ngonyani <zachariahn@gmail.com>
2024-09-02 15:47:05 +0300

    feat: create a lookup function to get coordinates

--- stories/Components/Map/lookup/index.js
+++ stories/Components/Map/lookup/index.js
@@ -1,6 +1,11 @@
 import data from "./data.json";
 
 export const getLatLong = (countryIsoCode) => {
-  const country = data.find((c) => c.country === countryIsoCode);
+  if (!countryIsoCode) return null;
+
+  const country = data.find(
+    (c) => c.country === countryIsoCode.substring(0, 2)
+  );
+
   return country ? [country.latitude, country.longitude] : null;
 };
