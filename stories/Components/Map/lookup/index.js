ed422242aa4bd5c55113f4132dead9917a659e8d
Ken Hawkins <kenneth.hawkins@un.org>
2024-09-05 20:07:58 +0200

    Reduce repetition

--- stories/Components/Map/lookup/index.js
+++ stories/Components/Map/lookup/index.js
@@ -1,25 +1,19 @@
 import data from "./data.json";
 
-export const getCountryCoords = (countryIsoCode) => {
-  // If there's nothing passed, return null
+const findCountryByIsoCode = (countryIsoCode) => {
   if (!countryIsoCode) return null;
 
-  // First, try to match by ISO alpha-3 code
-  const result = data.find(
+  return data.find(
     (c) => c.iso_alpha_3 === countryIsoCode.toUpperCase()
   );
-  
+};
+
+export const getCountryCoords = (countryIsoCode) => {
+  const match = findCountryByIsoCode(countryIsoCode);
   return match ? [match.latitude, match.longitude] : null;
 };
 
 export const getContinent = (countryIsoCode) => {
-  // If there's nothing passed, return null
-  if (!countryIsoCode) return null;
-
-  // First, try to match by ISO alpha-3 code
-  const match = data.find(
-    (c) => c.iso_alpha_3 === countryIsoCode.toUpperCase()
-  );
-
+  const match = findCountryByIsoCode(countryIsoCode);
   return match ? match.continent : null;
 };
