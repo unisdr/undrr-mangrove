0d24dfb2bb6305c727d134fe6e9c0f40b01903cf
Ken Hawkins <kenneth.hawkins@un.org>
2024-09-05 12:02:48 +0200

    #51: Add 3 letter ISO code for map.
    From https://unstats.un.org/unsd/methodology/m49/

--- stories/Components/Map/lookup/index.js
+++ stories/Components/Map/lookup/index.js
@@ -4,7 +4,7 @@ export const getLatLong = (countryIsoCode) => {
   if (!countryIsoCode) return null;
 
   const country = data.find(
-    (c) => c.country === countryIsoCode.substring(0, 2)
+    (c) => c.iso_alpha_3 === countryIsoCode.toUpperCase()
   );
 
   return country ? [country.latitude, country.longitude] : null;
