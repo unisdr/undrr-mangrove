17a91bea1071a7cc68320649ea1bdaa6ff45cde7
Zachariah Ngonyani <zachariahn@gmail.com>
2024-09-06 11:50:44 +0300

    fix: merge conflicts

--- stories/Components/Map/map-helpers.js
+++ stories/Components/Map/map-helpers.js
@@ -7,6 +7,21 @@ import { getContinent, getCountryCoords } from "./lookup";
 export const transformDataForMap = (results) => {
   const data = results
     .reduce((acc, entry) => {
+      // console.log("acc.country_iso_code",entry.country_iso_code);
+      // here we work around some geographic exception
+      // null = global
+      if (entry.country_iso_code == null) {
+        entry.country_name = "Global";
+        entry.title = "Global";
+        entry.country_id = "ALL";
+        entry.country_iso_code = "GLOBAL";
+      }
+      // false + country name = continent
+      if (entry.country_iso_code === false) {
+        entry.title = entry.country_name + " regional commitments";
+        entry.country_iso_code = entry.country_name.toUpperCase();
+        entry.country_name = entry.country_name;
+      }
       const existingEntry = acc.find(
         (item) => item.country_iso_code === entry.country_iso_code
       );
