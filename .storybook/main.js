695d5e1462a6b5c54d317fd0f3caeed7a3d6c657
Zachariah Ngonyani <zachariahn@gmail.com>
2024-09-11 07:45:44 +0300

    fix: update stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
 const path = require("path");
 
 module.exports = {
-  staticDirs: ['../stories/assets'],
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  //stories: ["../stories/**/CategoriesMenu2.stories.mdx"],
-  addons: [{ name: 'storybook-design-token', options: {
-    preserveCSSVars: true ,
-    // DESIGN_TOKEN_GLOB: "stories/assets/scss/.{scss,svg}"
-  } }, "@storybook/addon-links", "@storybook/addon-actions", "@storybook/addon-docs", "@storybook/addon-essentials", "@whitespace/storybook-addon-html", "storybook-addon-rtl", "@storybook/addon-a11y", "@etchteam/storybook-addon-css-variables-theme"],
+  staticDirs: ["../stories/assets"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
 
-  webpackFinal: async config => {
+  //stories: ["../stories/**/CategoriesMenu2.stories.mdx"],
+  addons: [
+    {
+      name: "storybook-design-token",
+      options: {
+        preserveCSSVars: true,
+        // DESIGN_TOKEN_GLOB: "stories/assets/scss/.{scss,svg}"
+      },
+    },
+    "@storybook/addon-viewport",
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@whitespace/storybook-addon-html",
+    "storybook-addon-rtl",
+    "@storybook/addon-a11y",
+    "@etchteam/storybook-addon-css-variables-theme",
+    "@storybook/addon-webpack5-compiler-babel",
+    "@chromatic-com/storybook",
+  ],
+
+  webpackFinal: async (config) => {
     // remove hash from the static file names
     // find the existing rule and override the name property
     // config.module.rules.forEach(function(rule, index) {
@@ -24,21 +41,24 @@ module.exports = {
     config.module.rules.push({
       test: /\.scss$/,
       exclude: /node_modules/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
     });
     return config;
   },
 
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
 
   docs: {
-    autodocs: false,
     story: {
       inline: false,
-    },
-  }
+    }
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
