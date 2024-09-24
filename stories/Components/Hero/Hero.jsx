b5d2ae3a833d2d9fa046607f078970668edc87b0
Zachariah Ngonyani <zachariahn@gmail.com>
2024-09-24 04:18:49 +0300

    fix: fix hero component's display issues
    - allow displaying of html tags inside the title
    - conditionally display links in header when present

--- stories/Components/Hero/Hero.jsx
+++ stories/Components/Hero/Hero.jsx
@@ -1,8 +1,5 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { CtaButton } from "../Buttons/CtaButton/CtaButton";
-// import './hero.scss';
-// import '../../../assets/scss/_grid.scss';
-// import imgPath from '../../../assets/images/hero_background.png';
 
 const cls = (...classes) =>
   classes.filter(Boolean).length > 0 ? classes.filter(Boolean).join(" ") : null;
@@ -24,37 +21,43 @@ export function Hero({ data, variant }) {
           className={cls("mg-hero", "mg-hero--" + `${variantActive}`)}
           style={{ backgroundImage: `url(${item.imgback})` }}
         >
-          <div key={index} className="mg-hero__overlay">
+          <div className="mg-hero__overlay">
             <article className="mg-hero__content">
               <div className="mg-hero__meta">
-                <a href={item.link} className="mg-hero__label">
-                  {item.label}
-                </a>
+                {item.label && (
+                  <a href={item.link || "#"} className="mg-hero__label">
+                    {item.label}
+                  </a>
+                )}
               </div>
 
               <header className="mg-hero__title">
-                <a href="#" className="text-xxl">
-                  {item.title}
-                </a>
+                {item.link ? (
+                  <a href={item.link} className="text-xxl">
+                    <span dangerouslySetInnerHTML={{ __html: item.title }} />
+                  </a>
+                ) : (
+                  <h1 className="text-xxl">
+                    <span dangerouslySetInnerHTML={{ __html: item.title }} />
+                  </h1>
+                )}
               </header>
 
-              <div className="mg-hero__summaryText">{item.summaryText}</div>
+              <div className="mg-hero__summaryText">
+                <span dangerouslySetInnerHTML={{ __html: item.summaryText }} />
+              </div>
+
               <div className="mg-hero__meta meta-detail">
-                <a href={item.link} className="mg-hero__label detail">
-                  {item.detail}
-                </a>
-                <a href={item.link} className="mg-hero__label detail">
-                  {item.detail}
-                </a>
+                {item.detail && (
+                  <a href={item.link || "#"} className="mg-hero__label detail">
+                    {item.detail}
+                  </a>
+                )}
               </div>
 
               <div className="mg-hero__buttons">
                 <CtaButton Type="Primary" label={item.primary_button} />
                 <CtaButton Type="Secondary" label={item.secondary_button} />
-
-                {/* <a href={item.link} className="mg-hero__button mg-hero__button-primary">{item.primary_button}</a> */}
-                {/* <a href={item.link} className="mg-hero__button mg-hero__button-secondary">{item.secondary_button}</a> */}
-                {/* <a href={item.link} className="mg-hero__button mg-hero__button-tertiary">{item.tertiary_button}</a> */}
               </div>
             </article>
           </div>
