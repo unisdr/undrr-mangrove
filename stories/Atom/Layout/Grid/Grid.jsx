365d568a5d7aa968a0979602a7abc9494c4707ab
Ken Hawkins <kenneth.hawkins@un.org>
2024-10-02 12:12:50 +0200

    Docs: simplify grid demonstration.

--- stories/Atom/Layout/Grid/Grid.jsx
+++ stories/Atom/Layout/Grid/Grid.jsx
@@ -5,24 +5,24 @@ export function Grid() {
     <>
       <h2>Auto Layout (mg-grid)</h2>
 
-      <div className="mg-grid">
+      <div className="mg-grid" style={{ border: '1px dashed #aaa' }}>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
@@ -32,26 +32,26 @@ export function Grid() {
       <h2>Specified columns layout</h2>
       <p>example with a two-column grid: mg-grid__col-2</p>
 
-      <div className="mg-grid mg-grid__col-2">
+      <div className="mg-grid mg-grid__col-2" style={{ border: '1px dashed #aaa' }}>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
 
-        <div>
+        <div style={{ border: '1px dashed #aaa' }}>
           <h3>UNDRR</h3>
           <p>UNDRR convenes partners and coordinates activities to create safer, more resilient communities.</p>
         </div>
 
-        <div className=" mg-grid__col--span-2">
+        <div className=" mg-grid__col--span-2" style={{ border: '1px dashed #aaa' }}>
           <h3>mg-grid__col--span-2</h3>
           <p>I use .mg-grid__col--span-2 to span 2 cols!</p>
         </div>
 
       </div>
 
-      <div className="mg-grid mg-grid__col-3">
+      <div className="mg-grid mg-grid__col-3" style={{ border: '1px dashed #aaa' }}>
       </div>
 
     </>
