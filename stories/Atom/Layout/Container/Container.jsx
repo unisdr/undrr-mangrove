bd4e0bcbde4b23c734f15f144f8fef25471443e1
Ken Hawkins <kenneth.hawkins@un.org>
2024-10-09 16:45:14 +0200

    Component: add basic mg-container.

---
+++ stories/Atom/Layout/Container/Container.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+// import './Container.scss'; // Import the SASS file
+
+export function Container({ children }) {
+  return <div className="mg-container">{children}</div>;
+}
