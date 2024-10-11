d44861a8a6a7f74ed8b55fb2327eeb4fac9c83db
Roberto Panzeri <robertopanzeri@rocketmail.com>
2024-10-11 17:08:12 +0200

    Styles for Header component WIP

--- stories/Components/MegaMenu/TopBar/TopBarItem.jsx
+++ stories/Components/MegaMenu/TopBar/TopBarItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Icons } from "../../../Atom/Icons/Icons";
-import information from "../../../assets/icons/information.svg"
 
 export function TopBarItem ({ title, onMouseEnter }) {
   return (
     <div className="mg-mega-topbar__item" onMouseEnter={onMouseEnter}>
       <svg aria-hidden="true" focusable="false" role="img">
+        {/* NOTE - Each menu item has a different icon, they are listed below (see design on Figma) */}
           <use href="icons/information.svg#information" />
+          {/* <use href="icons/remove-red-eye.svg#remove-red-eye" /> */}
+          {/* <use href="icons/help-outline.svg#help-outline" /> */}
+          {/* <use href="icons/settings.svg#settings" /> */}
       </svg>
       {title}
     </div>
