f99a823c03d5201518725178bfeb5927ff02b516
Roberto Panzeri <robertopanzeri@rocketmail.com>
2024-10-24 16:37:00 +0200

    Styles for Checkbox component - WIP

--- stories/Components/Forms/Checkbox/Checkbox.jsx
+++ stories/Components/Forms/Checkbox/Checkbox.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export function Checkbox({
-  label, value, arialabel, id, label_pos,
-}) {
-  return (
-      <label className="dts-form-field">
-        <input type="checkbox" name="" />
-        Checkbox label
-      </label>
-  );
+export function Checkbox({ label, value, arialabel, id, label_pos }) {
+    return (
+      <>
+        <div className="dts-form-field">
+          {/* Add aria-invalid="true" attribute to label tag when the field has ERRORS */}
+          <label aria-invalid="true">
+              {/* Add disabled attribute to the input tag to make it DISABLED */}
+              {/* Add  aria-describedBy="inputErrorId" attribute to the input tag when the field has ERRORS */}
+              <input type="checkbox" name="" aria-describedBy="inputErrorId" />
+              <span>Checkbox label</span>
+          </label>
+          <span className="dts-form-field__error-msg" id="inputErrorId" aria-live="assertive">
+              Error message goes here
+          </span>
+        </div>
+      </>
+    );
 }
