57b2073669f76869c483303384734351c6d444a4
Ken Hawkins <kenneth.hawkins@un.org>
2024-11-08 18:13:04 +0100

    Utility CSS: Create example reference and docs.

---
+++ stories/Utilities/UtilityCss/UtilityCss.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export function Utility () {
+  return (
+    <div className="utility-examples">
+      {/* Screen Reader Only Example */}
+      <p><code>.mg-u-sr-only</code></p>
+      <p>
+        <span className="mg-u-sr-only">This text is only visible to screen readers</span>
+        <span>This text is visible to everyone</span>
+      </p>
+
+      <hr/>
+
+      {/* Text Wrap Examples */}
+      <p><code>.mg-u-text-wrap-balanced</code></p>
+      <p className="mg-u-text-wrap-balanced">
+        <h2>This is an example of balanced text wrapping for pleasing line breaks</h2>
+      </p>
+      
+      <hr/>
+
+      <p><code>.mg-u-text-wrap-pretty</code></p>
+      <p className="mg-u-text-wrap-pretty">
+        This is an example of pretty text wrapping that optimizes readability. This is an example of pretty text wrapping that optimizes readability. This is an example of pretty text wrapping that optimizes readability.
+      </p>
+
+      <hr/>
+
+      <p><code>.mg-u-comma-between</code></p>
+      {/* Comma Separator Example */}
+      <p className="mg-u-comma-between">
+        <span>Item 1</span>
+        <span>Item 2</span>
+        <span>Item 3</span>
+      </p>
+    </div>
+  );
+};
---
+++ stories/Utilities/UtilityCss/UtilityCss.stories.jsx
@@ -0,0 +1,11 @@
+import { Utility } from "./UtilityCss";
+
+export default {
+  title: "Components/Utility CSS",
+  component: Utility,
+};
+
+export const DefaultUtility = {
+  render: () => <Utility />,
+  name: "Utility",
+};
