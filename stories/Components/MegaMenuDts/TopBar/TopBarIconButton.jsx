ddaa549b19c1ae45c7c63e4b174abb43b8077100
robertopanzeri <robertopanzeri@rocketmail.com>
2024-12-09 17:52:38 +0100

    MegaMenuDts component (#191)
    * MegaMenuDts component
    
    * Code linting

---
+++ stories/Components/MegaMenuDts/TopBar/TopBarIconButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icons } from "../../../Atom/Icons/Icons";
+
+export function TopBarIconButton({ onClick, icon }) {
+  return (
+    <button className="mg-mega-topbar__icon-button" onClick={onClick}>
+      <Icons 
+        src={icon}
+      />
+    </button>
+  )
+}
