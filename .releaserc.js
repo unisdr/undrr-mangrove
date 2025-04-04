6c022b0fea2cfede87fdb4ac85803a0dc5827269
Simon Vlcek <vlcek@cyber-guard.com>
2025-04-04 11:26:47 +0200

    Unable to use channel main for alpha

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: [{name: 'main', prerelease: 'alpha'}],
+    branches: ['main'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
