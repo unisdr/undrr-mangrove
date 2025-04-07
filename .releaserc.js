d25d7afb0437857b6ca335ce6eaf67264f56e223
Simon Vlcek <vlcek@cyber-guard.com>
2025-04-07 08:12:16 +0200

    Release workflow, PR title check

---
+++ .releaserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    branches: [
+      'main',
+    ],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      ['@semantic-release/git', {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }],
+      '@semantic-release/github'
+    ],
+  };
