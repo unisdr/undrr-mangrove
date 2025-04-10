cf4d515600e9e583426da7addefba0556a9e2e77
Simon Vlcek <vlcek@cyber-guard.com>
2025-04-10 08:50:15 +0200

    Test npm release

--- .releaserc.js
+++ .releaserc.js
@@ -6,6 +6,10 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/changelog',
+      ['@semantic-release/npm', {
+        npmPublish: false,
+        pkgRoot: '.',
+      }],
       ['@semantic-release/git', {
         assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
