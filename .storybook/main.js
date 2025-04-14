eb06c267dec89aa25930c709d3683f423500c84b
Simon Vlcek <vlcek@cyber-guard.com>
2025-04-14 11:20:44 +0200

    Surpress warnings

--- .storybook/main.js
+++ .storybook/main.js
@@ -62,6 +62,23 @@ export default {
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(currentDirPath, "../"),
     });
+    
+    if (!config.ignoreWarnings) {
+      config.ignoreWarnings = [];
+    }
+    
+    config.ignoreWarnings.push({
+      message: /Sass @import rules are deprecated/,
+    });
+    
+    config.ignoreWarnings.push({
+      message: /mixed-decls/,
+    });
+    
+    config.ignoreWarnings.push({
+      message: /repetitive deprecation warnings omitted/,
+    });
+    
     return config;
   },
 
