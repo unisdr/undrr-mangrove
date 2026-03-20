b0dae32d5d1eb77e19fa4cc32087ff18903e64d6
Ken Hawkins <kenneth.hawkins@un.org>
2026-03-20 07:00:31 +0100

    fix: Storybook regex for multi-segment legacy theme names (#836)
    The lazy-loading regex style(-\w+)?\.scss$ only matched one
    hyphen-word segment, so style-preventionweb-legacy.scss and
    similar files fell through to regular style-loader and broke
    the theme switcher. Changed to (-[\w-]+)? to match any number
    of segments.
    
    Also adds a division-by-zero guard to mg-rem() for the edge
    case where $mg-html-font-size is set to 0.
    
    Found by code review and developer advocacy review.

--- .storybook/main.js
+++ .storybook/main.js
@@ -54,7 +54,7 @@ export default {
     // SCSS support with lazy loading for theme files (enables hot reload + theme switching)
     // Theme files use lazyStyleTag so we can toggle them on/off
     config.module.rules.push({
-      test: /style(-\w+)?\.scss$/,
+      test: /style(-[\w-]+)?\.scss$/,
       exclude: /node_modules/,
       use: [
         { loader: 'style-loader', options: { injectType: 'lazyStyleTag' } },
@@ -67,7 +67,7 @@ export default {
     // Regular SCSS files (component styles, partials, etc.)
     config.module.rules.push({
       test: /\.scss$/,
-      exclude: [/node_modules/, /stories\/assets\/scss\/style(-\w+)?\.scss$/],
+      exclude: [/node_modules/, /stories\/assets\/scss\/style(-[\w-]+)?\.scss$/],
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(currentDirPath, '../'),
     });
